ea3a1fa4691134d18beb67779c36ef51a905748b
divesh_joshi <diveshjoshi0766@gmail.com>
2024-10-18 23:50:06 +0530

    creating sync between chirag and me

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,7 +2,6 @@ import {allNav} from './allNav';
 
 export const getNav = (role) => {
     const finalNavs = []
-
     for (let i = 0; i < allNav.length; i++) {
        if (role === allNav[i].role) {
          finalNavs.push(allNav[i])
