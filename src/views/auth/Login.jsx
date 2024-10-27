51c8bf557a6a5d9c2fbb61511ac35598f5dfc799
divesh_joshi <diveshjoshi0766@gmail.com>
2024-10-27 21:31:13 +0530

    testing

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center' >
           <div className='w-[350px] text-[#ffffff] p-2'>
             <div className='bg-[#6f68d1] p-4 rounded-md'>
-                <h2 className='text-xl mb-3 font-bold'>Welcome to Ecommerce</h2>
+                <h2 className='text-xl mb-3 font-bold'>Welcome to DIY Flex Realty</h2>
                 <p className='text-sm mb-3 font-medium'>Please Sing In your account</p>
 
     <form onSubmit={submit}>
--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -56,7 +56,7 @@ const Register = () => {
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center' >
           <div className='w-[350px] text-[#ffffff] p-2'>
             <div className='bg-[#6f68d1] p-4 rounded-md'>
-                <h2 className='text-xl mb-3 font-bold'>Welcome to Ecommerce</h2>
+                <h2 className='text-xl mb-3 font-bold'>Welcome to DIY Flex Realty</h2>
                 <p className='text-sm mb-3 font-medium'>Please register your account</p>
 
     <form onSubmit={submit}>
