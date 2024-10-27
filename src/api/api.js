a52eea811b311bab9d804b31e6fa0610cfe0d49f
divesh_joshi <diveshjoshi0766@gmail.com>
2024-10-27 20:14:39 +0530

    fixing routing

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+let url = process.env.SERVER_URL || "https://mlsserver-10ed9240e649.herokuapp.com";
 
 const api = axios.create({
-    baseURL : `${process.env.SERVER_URL}/api`
+    baseURL : `${url}/api`
 })
 
 export default api
