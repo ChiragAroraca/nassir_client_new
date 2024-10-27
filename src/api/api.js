3b65ed38bb51890eb92a80f58e7cf015a4652e24
divesh_joshi <diveshjoshi0766@gmail.com>
2024-10-27 21:12:27 +0530

    fixing

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let url = "https://mlsserver-10ed9240e649.herokuapp.com";
 
-console.log("url >><<", url)
+console.log("url >><< 123", url)
 
 const api = axios.create({
     baseURL : `${url}/api`
