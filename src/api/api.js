03c7836c5427447bde014f34430c7f998e712f06
Divesh Joshi <diveshjoshi0766@gmail.com>
2024-11-13 10:21:42 +0530

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const url = process.env.SERVER_URL || "http://localhost:5000";
 
+console.log("process.env.SERVER_URL >><<", process.env.SERVER_URL)
+
 const api = axios.create({
     baseURL: `${url}/api`,
     withCredentials: true 
