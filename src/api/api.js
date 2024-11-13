ddff13f68cdfde5651680b65ff088905f45ffabc
Divesh Joshi <diveshjoshi0766@gmail.com>
2024-11-13 10:15:51 +0530

    fixing

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
+const url = process.env.SERVER_URL || "http://localhost:5000";
 
 const api = axios.create({
     baseURL: `${url}/api`,
