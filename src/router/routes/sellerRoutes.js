b32c3d0d28bcfa7db340997b6d5f5c3b68e8e569
Divesh Joshi <diveshjoshi0766@gmail.com>
2025-01-08 09:08:37 +0530

    remove unwanted components

--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -1,24 +1,11 @@
 import { lazy } from 'react';
-import Uploads from '../../views/seller/Uploads';
-import TransactionManager from '../../views/seller/TransactionManager';
-
 const SellerDashboard = lazy(
   () => import('../../views/seller/SellerDashboard')
 );
 const AddProduct = lazy(() => import('../../views/seller/AddProduct'));
 const Products = lazy(() => import('../../views/seller/Products'));
-const DiscountProducts = lazy(
-  () => import('../../views/seller/DiscountProducts')
-);
-const Orders = lazy(() => import('../../views/seller/Orders'));
-const Payments = lazy(() => import('../../views/seller/Payments'));
-const SellerToAdmin = lazy(() => import('../../views/seller/SellerToAdmin'));
-const SellerToCustomer = lazy(
-  () => import('../../views/seller/SellerToCustomer')
-);
 const Profile = lazy(() => import('../../views/seller/Profile'));
 const EditProduct = lazy(() => import('../../views/seller/EditProduct'));
-const OrderDetails = lazy(() => import('../../views/seller/OrderDetails'));
 const Pending = lazy(() => import('./../../views/Pending'));
 const Deactive = lazy(() => import('./../../views/Deactive'));
 const AddBanner = lazy(() => import('../../views/seller/AddBanner'));
@@ -55,60 +42,6 @@ export const sellerRoutes = [
   {
     path: '/seller/dashboard/products',
     element: <Products />,
-    role: 'seller',
-    status: 'active',
-  },
-  {
-    path: '/seller/dashboard/discount-product',
-    element: <DiscountProducts />,
-    role: 'seller',
-    status: 'active',
-  },
-  {
-    path: '/seller/dashboard/orders',
-    element: <Orders />,
-    role: 'seller',
-    visibility: ['active', 'deactive'],
-  },
-  {
-    path: '/seller/dashboard/uploads',
-    element: <Uploads />,
-    role: 'seller',
-    visibility: ['active', 'deactive'],
-  },
-  {
-    path: '/seller/dashboard/transaction-manager',
-    element: <TransactionManager />,
-    role: 'seller',
-    visibility: ['active', 'deactive'],
-  },
-  {
-    path: '/seller/dashboard/order/details/:orderId',
-    element: <OrderDetails />,
-    role: 'seller',
-    visibility: ['active', 'deactive'],
-  },
-  {
-    path: '/seller/dashboard/payments',
-    element: <Payments />,
-    role: 'seller',
-    status: 'active',
-  },
-  {
-    path: '/seller/dashboard/chat-support',
-    element: <SellerToAdmin />,
-    role: 'seller',
-    visibility: ['active', 'deactive', 'pending'],
-  },
-  {
-    path: '/seller/dashboard/chat-customer/:customerId',
-    element: <SellerToCustomer />,
-    role: 'seller',
-    status: 'active',
-  },
-  {
-    path: '/seller/dashboard/chat-customer',
-    element: <SellerToCustomer />,
     role: 'seller',
     status: 'active',
   },
