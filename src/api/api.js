d0a2b85faf5b207b0d7de55cef19d9535bdd48d6
Divesh Joshi <diveshjoshi0766@gmail.com>
2025-01-09 23:44:18 +0530

    testing on vercel

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,6 @@ const url = process.env.REACT_APP_SERVER_URL || 'https://mlsserver-10ed9240e649.
 const api = axios.create({
   baseURL: `${url}/api`,
   withCredentials: true,
-});
+})
 
 export default api;
