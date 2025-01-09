9eb645e8933726909da2dc8617228c888839fafb
Divesh Joshi <diveshjoshi0766@gmail.com>
2025-01-10 00:02:25 +0530

    testing on vercel

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,4 +6,4 @@ export const overrideStyle = {
   justifyContent: 'center',
   alignItems: 'center',
 };
-export const socket = io('https://mlsserver-10ed9240e649.herokuapp.com');
+export const socket = io(process.env.REACT_APP_SERVER_URL);
