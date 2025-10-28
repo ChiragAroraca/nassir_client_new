b73a8dcc7322c87e477b3737343e4f4de221f918
ChiragAroraca <chiragaroraca10@gmail.com>
2025-10-29 00:32:55 +0530

    fix: Update axios instance baseURL to include API path

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
 // Create axios instance with interceptor
 import axios from 'axios';
 
+const url = process.env.REACT_APP_SERVER_URL || 'https://mlsserver-10ed9240e649.herokuapp.com';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: `${url}/api`,
   withCredentials: true,
 });
 
